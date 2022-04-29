e2e347c8db6fcbbdfd6a1aaddda47af53d2c5585
KZ-Lucas <qq3884qq@naver.com>
2022-04-29 22:03:34 +0900

    BinaryGap 풀이(주석 추가)

--- codility/Iterations/BinaryGap/최근원.js
+++ codility/Iterations/BinaryGap/최근원.js
@@ -1,8 +1,12 @@
 function solution(N) {
+  // 2 진수 변환
   const binary = N.toString(2);
+  // 0 최대 개수 저장용 변수
   let answer = 0;
+  // answer[] 배열
   const darry = [];
 
+  // 바이너리 Length만큼 돌면서 근접한 0의 개수만큼 배열에 저장
   for (let i = 0; i < binary.length; i++) {
     if (binary[i] === "1") {
       darry.push(answer);
@@ -12,5 +16,6 @@ function solution(N) {
     }
   }
 
+  // 배열 데이터 중 가장 큰 값을 리턴
   return Math.max(...darry);
 }
